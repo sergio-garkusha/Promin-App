5c34f9960c4ae53ed728da69ce97d560a67fff26
garys-primary <gary.sedletsky@gmail.com>
2022-05-09 10:37:09 -0700

    cleanup

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Text, View, StyleSheet, Button, Image } from 'react-native';
 
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-
 import WelcomeScreen from './screens/welcome'
 import MainMenu from './screens/mainMenu'
 import MainStack from './Routes/MainStack';
