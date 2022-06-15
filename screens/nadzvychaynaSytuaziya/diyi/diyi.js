61a03fb8feb6562aaa967631cc072f68ce8226e2
garys-primary <gary.sedletsky@gmail.com>
2022-06-14 22:16:57 -0700

    Added Air Raid page and started Accordeon component

--- screens/nadzvychaynaSytuaziya/diyi/diyi.js
+++ screens/nadzvychaynaSytuaziya/diyi/diyi.js
@@ -24,7 +24,7 @@ export default function Diyi({ navigation }) {
             <ListItem
               roundTop
               title="Вимкнули телебачення"
-              onPress={navigateAway}
+              onPress={()=>navigation.push("Telebachennya")}
             />
             <ListItem title="Повітряна тривога" onPress={navigateAway} />
             <ListItem title="Евакуація" onPress={navigateAway} />
