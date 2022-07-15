9794fe9886819e8ab19e890c121d602b6b5f59f1
garys-primary <gary.sedletsky@gmail.com>
2022-07-14 18:24:53 -0700

    Added accordion
    Needs more work:
    - Icon + it's animation,
    - Props for 2 texts
    - adaptive style

--- components/Accordion.js
+++ components/Accordion.js
@@ -2,46 +2,70 @@ import React, { useRef, useEffect } from 'react';
 import { Animated, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-let toggleDrawer = () => {
-
-}
-
-export default function Accordion() {
-  const drawerAnim = useRef(new Animated.Value(0)).current
-
-  useEffect(() => {
-    Animated.timing(
-      drawerAnim,
-      {
-        toValue: 100,
-        duration: 500,
-      }
-    ).start();
-  }, [drawerAnim])
+export default function App() {
+  const drawerAnim = useRef(new Animated.Value(0)).current  
+  let textRef = useRef(null);
+  let textHeight = useRef(0);
+  let isOpen = useRef(false);
   
+  useEffect(() => {
+    textRef.current.measure((fx, fy, width, height, px, py) => {
+      textHeight.current = height
+    });
+  });
+
+  const openDrawer = () => {
+    Animated.timing(                  // Animate over time
+      drawerAnim,            // The animated value to drive
+      {
+        toValue: textHeight.current,                
+        duration: 500,
+        useNativeDriver: false
+      }
+    ).start()
+  }
+  const closeDrawer = () => {
+    Animated.timing(                  // Animate over time
+      drawerAnim,            // The animated value to drive
+      {
+        toValue: 0,                
+        duration: 500,
+        useNativeDriver: false
+      }
+    ).start()
+  }
+  const toggleDrawer = () => {
+    if(isOpen.current)
+      closeDrawer()
+    else
+      openDrawer()
+    isOpen.current = !isOpen.current
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.headerPanel}>
-        <Text style={styles.headerText}>
-          element title sd sadfcascas casdasxca sasasc ascasc ascas casc asc asc asc 
-        </Text>
-        <TouchableOpacity style={styles.downIcon} onPress={toggleDrawer}/>
+      <View style={styles.accordeon}>
+        <View style={styles.headerPanel}>
+          <Text style={styles.headerText}>
+            element title sd 
+          </Text>
+          <TouchableOpacity style={styles.downIcon} onPress={toggleDrawer}/>
+        </View>
+        
+        <Animated.View style={[styles.bodyPanel, {height: drawerAnim}]}>
+          <View style={styles.divider}/>
+          <View ref={textRef} style={{height:'auto'}}>
+            <Text style={styles.bodyText}>
+              thiss is article text blakdvmoa fdv dsvln sdvkjnfv sdvknzd  adsvoij asdovm adovn advoknaofv 
+            </Text>
+          </View>
+        </Animated.View>
       </View>
-      <View style={styles.divider}/>
-      <Animated.View style={[styles.bodyPanel, {height: drawerAnim}]}>
-        <Text style={styles.bodyText}>
-          thiss is article text blah blah ESFlijsfn sdvzdsvln sdvkjnfv sdvknzd  adsvoij asdovm adovn advoknaofv afknv avoknaokvmaokvmoa v aodsfnvao kvmoakvm aokdvmoa fdv
-        </Text>
-      </Animated.View>
     </View>
   );
 }
+
+const backgroundColor = "#FFFFFF"
 
 const styles = StyleSheet.create({
   container: {
@@ -51,11 +75,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
+  accordeon:{
+    borderRadius:12,
+    padding:4,
+    backgroundColor: backgroundColor
+  },
   headerPanel: {
     fontWeight: 'bold',
     textAlign: 'left',
-    backgroundColor: "#FFFFFF",
-    flexDirection: 'row'
+    backgroundColor: backgroundColor,
+    flexDirection: 'row',
+    overflow:"hidden"
   },
   headerText: {
     fontWeight: 'bold',
@@ -69,18 +99,19 @@ const styles = StyleSheet.create({
     width:20,
     marginTop:'auto',
     marginBottom:'auto',
-    marginRight:12
+    marginRight:12,
+    borderRadius: 7
   },
   divider:{
     height:1,
     backgroundColor: "#DDDDDD"
   },
   bodyPanel: {
-    backgroundColor:'#FFFFFF'
+    backgroundColor:'#FFFFFF',
+    overflow:'hidden'
   },
   bodyText: {
     fontSize:16,
     padding:16,
-    overflow:'hidden'
   }
 });
