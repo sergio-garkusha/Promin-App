44e227fe3387fb384c8ee63d5704890d3cc08de6
Keeppie <kipish92@inbox.ru>
2022-07-23 00:00:19 -0700

    Added Accordion to Techniky and Tilesni

--- screens/psychologichnaDopomoga/dlyaSebe/dlyaSebeScreens/techniky.js
+++ screens/psychologichnaDopomoga/dlyaSebe/dlyaSebeScreens/techniky.js
@@ -4,6 +4,9 @@ import Header from "/components/Header";
 import { ThemeContext } from "/components/ThemeProvider";
 import { FontSizeContext } from "/components/FontSizeProvider";
 import ListItem from "/components/ListItem";
+import Accordion from "../../../../components/Accordion";
+import Bullet from "/components/Bullet";
+import resolveStyles from "/styles/subpage";
 
 const navigateAway = () => {
   // eslint-disable-next-line no-console
@@ -21,11 +24,88 @@ export default function Techniky({ navigation }) {
       <ScrollView>
         <View style={styles.container}>
           <View style={{ padding: 20 }}>
-            <Text style={styles.title}>Техніки самодопомоги</Text>
-            <ListItem roundTop title="Дихальні вправи" onPress={navigateAway} />
-            <ListItem title="Тілесні вправи" onPress={navigateAway} />
-            <ListItem title="Когнітивні вправи"onPress={navigateAway}/>
-            <ListItem roundBottom title="Релаксаційні вправи" onPress={navigateAway}/>
+            <Text style={styles.title}>Техніки самодопомоги в моменті</Text>
+            <Text style={styles.paragraph}>У важкі часи кожна людина потребує повернення до внутрішньої опори та 
+            саморозуміння. Пропонуємо техніки та методи саморозуміння та самодопомоги.</Text>
+
+            <Accordion title= "Щоденник думок"> 
+              <Text style={styles.paragraph}>Прислухайтесь до себе, своїх думок, своїх почуттів, своїх відчуттів.
+              Запишіть “почуте”:</Text>   
+          
+              <Bullet symbol="•" style={styles.paragraph}>
+              Гуляйте, коли безпечно </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              Створюйте рутину </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              Проводьте якомога більше часу з тими кого ви любите  </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              Ліміт на новини </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              Допомагайте тим, хто потребує (станьте важливим для когось) </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              Тримайтеся подалі від катастрофічних думок (свідомо переключайте свої думки з катастрофічних), 
+              не розмовляйте з думками яких ви “не запрошували” </Bullet>
+
+            </Accordion>
+
+            <Accordion title= "Техніка роботи з тривожними переживаннями"> 
+              <Text style={styles.paragraph}>Відповідайте на питання, рухаючись за схемою:</Text>   
+          
+              <Bullet symbol="1." style={styles.paragraph}>
+              Про що саме я переживаю? </Bullet>
+
+              <Bullet symbol="2." style={styles.paragraph}>
+              Ця проблема наразі є актуальною та я маю можливість її вирішити?
+              
+              <Bullet symbol="•" style={styles.paragraph}>
+              якщо ТАК - розробити покроковий план вирішення проблеми </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              якщо НІ - перевести увагу та зробити корисну активність </Bullet>
+              
+               </Bullet>
+
+              <Bullet symbol="3." style={styles.paragraph}>
+              Який крок плану я зараз можу зробити? 
+              
+              <Bullet symbol="•" style={styles.paragraph}>
+              якщо МОЖУ зробити крок з плану - виконайте цей крок та переведіть увагу на корисну активність </Bullet>
+
+              <Bullet symbol="•" style={styles.paragraph}>
+              якщо НЕ МОЖУ зробити крок з плану - заплануйте коли і як Ви зможете його зробити та та переведіть 
+              увагу на корисну активність </Bullet>
+              
+              </Bullet>
+
+              <Text style={styles.paragraph}>Корисною активністю можуть бути будь-які рутинні та побутові справи.</Text>   
+
+            </Accordion>
+          
+            <Text style={styles.title}>Тілесні техніки</Text>
+          
+            <ListItem roundTop title="Відчуваю своє тіло" onPress={navigateAway} />
+            <ListItem title="Техніка білатеральної стабілізації “Обійми метелика” " onPress={navigateAway} />
+            <ListItem title="Техніка білатеральної стабілізації – білатеральні поплескування"onPress={navigateAway}/>
+            <ListItem roundBottom title="Тілесна техніка “Десенситизація”" onPress={navigateAway}/>
+
+            <Text style={styles.title}>Дихальні техніки</Text>
+
+            <ListItem title="Дихальна техніка стабілізації"onPress={navigateAway}/>
+            <ListItem roundBottom title="Дихальна техніка “3-5-7”" onPress={navigateAway}/>
+
+            <Text style={styles.title}>Техніки заземлення</Text>
+
+            <ListItem roundTop title="Техніка заземлення за відчуттями ніг" onPress={navigateAway} />
+            <ListItem title="Техніка заземлення в положенні лежачи" onPress={navigateAway} />
+            <ListItem title="Техніка заземлення в русі"onPress={navigateAway}/>
+            <ListItem roundBottom title="Техніка заземлення з відтворенням ритму" onPress={navigateAway}/>
+            <ListItem roundBottom title="Техніка заземлення через активні дії" onPress={navigateAway}/>
+
           </View>
         </View>
       </ScrollView>
